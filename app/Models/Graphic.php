ac129ec49b1b77d82ad4cb0976af9899f9e10249
NesCh99 <neschp@gmail.com>
2023-06-05 20:17:31 -0500

    First commit

---
+++ app/Models/Graphic.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Graphic extends Model
+{
+    use HasFactory;
+
+    public const SPROTT = 1;
+    public const LORENZ = 2;
+    public const CHEN = 3;
+    public const ROSSLER = 4;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'parameters',
+        'results',
+        'type',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'parameters' => 'array',
+        'results' => 'array',
+        'type' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getSprottGraphics()
+    {
+        return $this->where('type',self::SPROTT)->get();
+    }
+}
---
+++ database/seeders/GraphicSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GraphicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
